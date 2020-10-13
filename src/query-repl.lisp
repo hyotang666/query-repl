c83d5bb01aea8bb54beb04fc2b276f199ac8ee86
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-10-13 15:29:01 +0900

    Add: PPRINT-QUERY-CASE.

--- src/query-repl.lisp
+++ src/query-repl.lisp
@@ -64,3 +64,43 @@
 (defmacro query-case (query &body clauses)
   `(pcs:restart-case (progn ,query (force-output *query-io*) (query-repl))
      ,@clauses))
+
+(defun pprint-query-case (stream exp)
+  (funcall
+    (formatter
+     #.(concatenate 'string "~:<" ; pprint-logical-block.
+                    "~W~^ ~1I~@_" ; operator.
+                    "~W~^ ~_" ; form.
+                    "~@{" ; clauses.
+                    "~/query-repl:pprint-query-case-clause/~^ ~_" ; each-clause
+                    "~}" "~:>"))
+    stream exp))
+
+(defun pprint-query-case-clause (stream exp &rest noise)
+  (declare (ignore noise))
+  (if (atom exp)
+      (write exp :stream stream)
+      (pprint-logical-block (stream exp :prefix "(" :suffix ")")
+        (pprint-exit-if-list-exhausted)
+        (apply
+          (formatter
+           #.(concatenate 'string "~{~W~^ ~@_~:<~^~W~:>~}" ; pre.
+                          "~@[" ; if exists.
+                          " ~3I~_~{~W~^ ~@_~W~^ ~_~}" ; keys
+                          "~]" "~^ ~1I" ; if exists body.
+                          "~:*~:[~_~;~:@_~]" ; mandatory newline when keys.
+                          "~@{~W~^ ~:@_~}")) ; body.
+          stream (parse-query-clause exp)))))
+
+(defun parse-query-clause (clause)
+  (let ((pre
+         (cons (car clause)
+               (when (cdr clause)
+                 (list (cadr clause))))))
+    (loop :for list :on (cddr clause) :by #'cddr
+          :while (and (keywordp (car list)) (cdr list))
+          :collect (car list) :into keys
+          :collect (cadr list) :into keys
+          :finally (return (list* pre keys list)))))
+
+(set-pprint-dispatch '(cons (member query-case)) 'pprint-query-case)
