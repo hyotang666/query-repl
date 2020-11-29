14b202dede91e3d88c007eb6d245a535c8b270a6
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-11-30 01:01:10 +0900

    Add: Spec for QUERY-EVAL.

--- spec/query-repl.lisp
+++ spec/query-repl.lisp
@@ -91,3 +91,59 @@ input> "
 
 ;;;; Exceptional-Situations:
 
+(requirements-about QUERY-EVAL :doc-type function)
+
+;;;; Description:
+
+#+syntax (QUERY-EVAL exp) ; => result
+
+;;;; Arguments and Values:
+
+; exp := T
+
+; result := T
+
+;;;; Affected By:
+; *QUERY-EVAL*
+; When *QUERY-EVAL* is NIL, EXP itself is returned.
+#?(query-eval '(+)) => 0
+#?(let ((*query-eval*))
+    (query-eval '(+)))
+=> (+)
+,:test equal
+
+; QUERY-REPL::*SELECTIONS* internal use.
+; When EXP is integer and can get (nth EXP QUERY-REPL::*SELECTIONS*)
+; such selections interactive-function is called.
+#?(query-bind ((test (lambda () (princ :will-printed))))
+    (query-eval 0))
+:outputs "WILL-PRINTED"
+
+; When EXP is symbol and can get only one slection from QUERY-REPL::*SELECTION* by its name
+; such selections interactive-function is called.
+#?(query-bind ((test (lambda () (princ :will-printed))))
+    (query-eval :test))
+:outputs "WILL-PRINTED"
+
+; When EXP is symbol and some selections are selected from QUERY-REPL::*SELECTION* by name
+; a warning is signaled.
+#?(query-bind ((test (lambda () (princ :never)))
+               (test2 (lambda () (princ :never))))
+    (query-eval :test))
+:signals warning
+
+; In such case, (values) are returned.
+#?(query-bind ((test (lambda () (princ :never)))
+               (test2 (lambda () (princ :never))))
+    (query-eval :test))
+:values ()
+,:ignore-signals warning
+
+;;;; Side-Effects:
+; Refer *QUERY-EVAL*
+; Modify +++ ++ + /// // / *** ** *.
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
