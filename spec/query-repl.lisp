9c9c3abfb9f2de230bf3f643bfba7b5c34b4de95
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-11-30 00:26:21 +0900

    Add: Spec for *PROMPT*.

--- spec/query-repl.lisp
+++ spec/query-repl.lisp
@@ -3,3 +3,17 @@
 (in-package :query-repl.spec)
 (setup :query-repl)
 
+(requirements-about *PROMPT* :doc-type variable)
+
+;;;; Description:
+
+;;;; Value type is T.
+#? *PROMPT* :be-the T
+
+; Initial value is `">"`
+
+;;;; Affected By:
+; QUERY-PROMPT
+
+;;;; Notes:
+
